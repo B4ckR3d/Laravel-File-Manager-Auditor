370537cd98e0e0fcb6ef972bc15dd70b1b077d06
B4ckR3d <34152674+B4ckR3d@users.noreply.github.com>
2026-03-09 16:22:20 +0700

    Create banner.py

---
+++ banner.py
@@ -0,0 +1,10 @@
+def show_banner():
+
+    banner = r"""
+╔══════════════════════════════════════╗
+║   Laravel File Manager Auditor v2   ║
+║   High-Speed Security Scanner       ║
+╚══════════════════════════════════════╝
+"""
+
+    print(banner)
